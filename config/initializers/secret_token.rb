885595d2050d6e7d4aee9a8d8bf1e8402b5146e1
wafamuchtar <wafamuchtar@gmail.com>
2010-11-23 15:01:28 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myupload::Application.config.secret_token = '54000a785fff47896f461343766a6e4e57341f29a1d380c6d2d07bf3e59d5da190b640771b4f7f35606034ebc4b246ef7fda569a7cc186e23df657b8a3596597'
